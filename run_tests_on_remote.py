39c575375d5979f61fb61cc98d69c83b8bff1ed1
Taylor Isbell <taylor.isbell@databricks.com>
2024-09-25 14:43:00 -0400

    updated action and added test runner

---
+++ run_tests_on_remote.py
@@ -0,0 +1,39 @@
+import os
+
+import pytest
+from databricks.sdk import WorkspaceClient
+
+
+def get_cluster_id():
+
+    w = WorkspaceClient()
+
+    clusters = list(w.clusters.list())
+
+    testing_cluster_name = "Testing Cluster"
+
+    matched_cluster = [x for x in clusters if x.cluster_name == testing_cluster_name]
+
+    if len(matched_cluster) > 1:
+        raise Exception("Duplicate cluster names!")
+    elif len(matched_cluster) == 0:
+        cluster = w.clusters.create_and_wait(
+            cluster_name=testing_cluster_name,
+            spark_version="15.4.x-scala2.12",
+            node_type_id="Standard_D4ds_v5",
+            autotermination_minutes=30,
+            enable_elastic_disk=True,
+            single_user_name=w.current_user.me().user_name,
+            num_workers=0,
+        )
+        cluster_id = cluster.cluster_id
+    else:
+        cluster_id = matched_cluster[0].cluster_id
+        w.clusters.ensure_cluster_is_running(cluster_id=cluster_id)
+    return cluster_id
+
+
+if __name__ == "__main__":
+    cluster_id = get_cluster_id()
+    os.environ["DATABRICKS_CLUSTER_ID"] = cluster_id
+    pytest.main()
